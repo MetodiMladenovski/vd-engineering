7d77ce8975e3161b2bd37ba63e442a93cc43fdd4
Metodi Mladenovski <meto.18@outlook.com>
2023-08-06 16:37:18 +0200

    Finished nav, half front page

---
+++ main.js
@@ -0,0 +1,26 @@
+var slideIndex = 0;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    let i;
+    let x = document.getElementsByClassName("mySlides");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length}
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex-1].style.display = "block";
+}
+
+function currentDiv(number) {
+    slideIndex = number;
+    let x = document.getElementsByClassName("mySlides");
+    for (let i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[number].style.display = "block";
+}
